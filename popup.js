91cc6289c97a20be059f32a17f50c2d26860dc37
utmichelle <54852282+utmichelle@users.noreply.github.com>
2019-11-03 09:57:02 -0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     var resultsButton = document.getElementById("getResults");
-    resultsButton.onclick = getResults();
+    resultsButton.onclick = getResults;
 });
 
 function getResults(){
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { action: "checkForWord" }, function (response) {
-            showResults(response.results);
+            showResults(response);
         });
     });
 }
